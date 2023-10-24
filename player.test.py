8d48be6c1bb459f06c816419a8a8f3d4b5953887
Poypoy17 <alexandre-poix@laposte.net>
2023-10-24 14:32:17 +0200

    Initial commit

--- player.test.py
+++ player.test.py
@@ -1,16 +1,16 @@
 from player import Player
 
-player = Player('X')
+PLAYER = Player('X')
 
 def test_get_char():
-    assert player.get_char() == 'X'
+    assert PLAYER.get_char() == 'X'
 
 def test_get_token():
-    assert player.get_token() == True
+    assert PLAYER.get_token() == True
 
 def test_set_tokenFalse():
-    player.set_tokenFalse()
-    assert player.get_token() == False
+    PLAYER.set_tokenFalse()
+    assert PLAYER.get_token() == False
 
 def test_player():
     test_get_char()
