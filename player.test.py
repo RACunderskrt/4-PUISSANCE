b8f864aa32d28c62489c51527043e1e9129c2369
RAC <thomas.humbert.2@etu.univ-amu.fr>
2023-11-16 14:49:33 +0100

    Ajout test player - verif input

--- player.test.py
+++ player.test.py
@@ -12,9 +12,26 @@ def test_set_tokenFalse():
     PLAYER.set_tokenFalse()
     assert PLAYER.get_token() == False
 
+def test_verify_token_error_true():
+    PLAYER.set_tokenFalse()
+    assert PLAYER.verify_token_error("5R") == True
+
+def test_verify_token_error_false():
+    assert PLAYER.verify_token_error("5") == False and PLAYER.verify_token_error("5R")
+
+def test_verify_token_error_false():
+    assert PLAYER.verify_token_error("5R") == False
+
+
+
 def test_player():
     test_get_char()
     test_get_token()
     test_set_tokenFalse()
+    PLAYER.set_tokenTrue()
+    test_verify_token_error_true()
+    PLAYER.set_tokenTrue()
+    test_verify_token_error_false()
+
 
 test_player()
