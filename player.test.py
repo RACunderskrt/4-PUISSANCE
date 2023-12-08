f976cc2a1734d2d88714d61924aa507d0a4c72a7
h21207482 <thomas.humbert.2@etu.univ-amu.fr>
2023-12-08 23:03:06 +0100

    ajout test player

--- player.test.py
+++ player.test.py
@@ -1,9 +1,12 @@
 from player import Player
 
-PLAYER = Player('X')
+PLAYER = Player('X', 1)
 
-def test_get_char():
-    assert PLAYER.get_char() == 'X'
+def test_get_name():
+    assert PLAYER.get_name() == 'X'
+
+def test_get_id():
+    assert PLAYER.get_id() == 1
 
 def test_get_token():
     assert PLAYER.get_token() == True
@@ -12,20 +15,18 @@ def test_set_tokenFalse():
     PLAYER.set_tokenFalse()
     assert PLAYER.get_token() == False
 
-def test_verify_token_error_true():
+def test_verify_token_error_true(): #the token is valid
     PLAYER.set_tokenFalse()
-    assert PLAYER.verify_token_error("5R") == True
+    assert PLAYER.verifyToken("5R")
 
-def test_verify_token_error_false():
-    assert PLAYER.verify_token_error("5") == False and PLAYER.verify_token_error("5R")
+def test_verify_token_error_false(): #the token is invalid
+    assert not PLAYER.verifyToken("5R")
 
-def test_verify_token_error_false():
-    assert PLAYER.verify_token_error("5R") == False
 
 
 
 def test_player():
-    test_get_char()
+    test_get_name()
     test_get_token()
     test_set_tokenFalse()
     PLAYER.set_tokenTrue()
